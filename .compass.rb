8d070d8409849d7832255dfdcc6893196409e804
Mark Jones <mark@thisismarkup.co.uk>
2013-02-11 23:17:00 +0000

    Added compass-normalize config

---
+++ .compass.rb
@@ -0,0 +1,6 @@
+require 'compass-normalize'
+
+css_dir = 'temp/styles'
+sass_dir = 'app/styles'
+images_dir = 'app/images'
+javascripts_dir = 'temp/scripts'
--- Gruntfile.js
+++ Gruntfile.js
@@ -32,10 +32,7 @@ module.exports = function( grunt ) {
       dist: {
         // http://compass-style.org/help/tutorials/configuration-reference/#configuration-properties
         options: {
-          css_dir: 'temp/styles',
-          sass_dir: 'app/styles',
-          images_dir: 'app/images',
-          javascripts_dir: 'temp/scripts',
+          config: '.compass.rb',
           force: true
         }
       }
