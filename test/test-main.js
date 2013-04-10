49bf5b735a74fd7f3fae8bfdb859a3137c4e07db
Mark Jones <mark.jones@kaldorgroup.com>
2013-04-10 11:03:59 +0100

    Testing config

---
+++ test/test-main.js
@@ -0,0 +1,20 @@
+var tests = Object.keys(window.__karma__.files).filter(function (file) {
+  return /\.test\.js$/.test(file);
+});
+
+require.config({
+  baseUrl: '/base',
+  paths: {
+    jquery: 'app/components/jquery/jquery',
+    chai: 'test/lib/chai',
+    sinon: 'test/lib/sinon'
+  },
+  shim: {}
+});
+
+require({
+  deps: tests,
+  callback: window.__karma__.start
+});
+
+window.log=function(){log.history=log.history||[];log.history.push(arguments);if(this.console){console.log(Array.prototype.slice.call(arguments));}};
