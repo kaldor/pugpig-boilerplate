7a0ab810b00854bccd617ce61448d463063ad4d1
Mark Jones <mark.jones@kaldorgroup.com>
2013-04-10 11:08:06 +0100

    Use compass config file

--- Gruntfile.js
+++ Gruntfile.js
@@ -114,14 +114,8 @@ module.exports = function (grunt) {
         },
         compass: {
             options: {
-                sassDir: '<%= yeoman.app %>/styles',
-                cssDir: '.tmp/styles',
-                imagesDir: '<%= yeoman.app %>/images',
-                javascriptsDir: '<%= yeoman.app %>/scripts',
-                fontsDir: '<%= yeoman.app %>/styles/fonts',
-                importPath: 'app/components',
-                relativeAssets: true
-            },
+                config: '.compass.rb'
+              },
             dist: {},
             server: {
                 options: {
